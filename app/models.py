4b559707b82bc32a2a0f5abc10ce190a90a9de7e
Almaz Galimov <almazwork@gmail.com>
2024-08-14 15:56:34 +0500

    add models

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    create_at = Column(DateTime, auto_now_add=True)
+    update_at = Column(DateTime, auto_now=True)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
